1ad9076f76c6b4df690b55b661de02a6331d000b
Martin Tech <75930741+MartinTech63@users.noreply.github.com>
2025-04-04 18:46:31 +0200

    maj

--- SCRIPTS/popup.js
+++ SCRIPTS/popup.js
@@ -2,10 +2,8 @@ window.onload = function () {
   const popup = document.getElementById("popupOverlay");
   popup.style.display = "flex";
 
-  // Empêche le scroll
   document.body.classList.add("noscroll");
 
-  // Déclenche l'animation de fondu après un petit délai
   setTimeout(() => {
     popup.classList.add("show");
   }, 10);
@@ -15,10 +13,8 @@ function closePopup() {
   const popup = document.getElementById("popupOverlay");
   popup.classList.remove("show");
 
-  // Retire la classe qui bloque le scroll
   document.body.classList.remove("noscroll");
 
-  // Cache le popup après la transition
   setTimeout(() => {
     popup.style.display = "none";
   }, 500);
